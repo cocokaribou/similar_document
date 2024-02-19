bab43334a228529a77139de7404404e6d7fc455c
cocokaribou <younginlee2017@gmail.com>
2024-02-19 15:11:41 +0900

    [Update] refactor [TODO] jvm error

--- api/d2v_model_inference.py
+++ api/d2v_model_inference.py
@@ -1,21 +1,38 @@
-from konlpy.tag import Okt
+from elasticsearch import Elasticsearch
+
+from konlpy.tag import Okt, Kkma
 from gensim.models import Doc2Vec
 
-from es import es
+from config import HOST, PORT, USER, PW
 
 if __name__ == "__main__":
     # korean word tokenizer
-    okt = Okt()
+    kkm = Kkma()
 
     # load model
     model = Doc2Vec.load("./doc2vecmodel.mod")
 
-    query = {"query": {"match_all": {}}}
+    query = {
+        "query": {
+            "bool": {
+                "filter": {
+                    "script": {
+                        "script": "doc['contents'].size() > 300"
+                    }
+                }
+            }
+        }
+    }
+    es = Elasticsearch(
+        hosts=[{'host': HOST, 'port': PORT, 'scheme': "https"}],
+        request_timeout=300, max_retries=10, retry_on_timeout=True,
+        basic_auth=(USER, PW)
+    )
     docs = es.search(index="dschool", body=query, size=10000)["hits"]["hits"]
 
     for doc in docs:
         text = doc["_source"]["contents"]
-        tokens = okt.morphs(text)
+        tokens = kkm.morphs(text)
         vector = model.infer_vector(tokens)
         es.update(index="dschool", id=doc["_id"], body={"doc": {"vec": vector}})
 
--- api/d2v_model_train.py
+++ api/d2v_model_train.py
@@ -22,28 +22,17 @@ class LossCallback(CallbackAny2Vec):
 
 if __name__ == "__main__":
     # korean word tokenizer
-    # okt = Okt()
-
-    os.environ["JAVA_HOME"] = "/Library/Java/JavaVirtualMachines/jdk1.8.0_311.jdk/Contents/Home"
-
     kkm = Kkma()
 
-    text = "왜 안되세요"
-    print(kkm.morphs(text, stem=True))
-    # get contents over 500 words (pre-processing)
-    # documents = es.get_contents_over_500_words()
+    documents = es.get_contents_over_500_words()
 
     # tagged data
-    # okt.morphs(doc) -> kkm.nouns(doc)
-    # tagged_data = [TaggedDocument(words=kkm.nouns(doc), tags=[idx]) for idx, doc in enumerate(documents)]
-    # tagged_data = [x for x in tagged_data if not x == "학원"]
+    tagged_data = [TaggedDocument(words=kkm.morphs(doc), tags=[idx]) for idx, doc in enumerate(documents)]
 
-    # print("\n".join(tagged_data))
-    #
     # # Doc2Vec 모델 생성 및 훈련
-    # model = Doc2Vec(vector_size=50, window=2, min_count=1, dm=1, workers=4)
-    # model.build_vocab(tagged_data)
-    # model.random.seed(0)
-    # model.train(tagged_data, total_examples=model.corpus_count, epochs=100, callbacks=[LossCallback()])
-    #
-    # model.save("./doc2vecmodel.mod")
+    model = Doc2Vec(vector_size=50, window=2, min_count=1, dm=1, workers=4)
+    model.build_vocab(tagged_data)
+    model.random.seed(0)
+    model.train(tagged_data, total_examples=model.corpus_count, epochs=100, callbacks=[LossCallback()])
+
+    model.save("./doc2vecmodel.mod")
